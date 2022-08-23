36b19b6a998e09af52498d52c9c45808bed27e20
akazus <kazus0105@mail.ru>
2022-08-24 02:06:41 +0400

    fix: add header to coinList table

--- src/Models/CoinsList.ts
+++ src/Models/CoinsList.ts
@@ -1,19 +1,23 @@
 import { CoinMarketData } from '../api/apiRequestTypes';
 import { getCoinMarketData } from '../api/apiRequest';
+import { CoinOrder } from '../constants';
 
 export default class CoinsList {
 
     public coinsListFromApi: Array<CoinMarketData>;
+
+    public sortOption: CoinOrder;
 
     public currency: string;
 
     constructor() {
         this.coinsListFromApi = [];
         this.currency = 'USD';
+        this.sortOption = CoinOrder.MARKET_CAP_DESC;
     }
 
     async apiReqArray() {
-        this.coinsListFromApi = await getCoinMarketData(this.currency);
+        this.coinsListFromApi = await getCoinMarketData(this.currency, this.sortOption);
         return this.coinsListFromApi;
     }
 
--- src/Views/CoinsListView.ts
+++ src/Views/CoinsListView.ts
@@ -22,17 +22,40 @@ export default class CoinsListView {
     }
 
     viewAllCoins() {
-        this.controller.coinsList.coinsListFromApi.forEach((oneCoin: CoinMarketData) => {
-            const coinHTML: HTMLElement = createNewElement('div', ['one_coin_container'], this.coinsListHTML);
+        const headerRow: HTMLElement = createNewElement('div', ['header_container'], this.coinsListHTML);
 
-            CoinsListView.addCoinDesctiptionHTML(oneCoin.name, coinHTML);
-            CoinsListView.addCoinDesctiptionHTML(oneCoin.current_price, coinHTML);
-            CoinsListView.addCoinDesctiptionHTML(oneCoin.price_change_24h, coinHTML);
+        const numberCoinHeader = createNewElement('div', ['coin-number-header'], headerRow);
+        numberCoinHeader.textContent = '#';
+        const nameCoinHeader = createNewElement('div', ['coin-name-header'], headerRow);
+        nameCoinHeader.textContent = 'Наименование';
+        const currentPriceHeader = createNewElement('div', ['coin-price-header'], headerRow);
+        currentPriceHeader.textContent = 'Цена';
+        const changePriceHeader = createNewElement('div', ['coin-change-price-header'], headerRow);
+        changePriceHeader.textContent = '24h %';
+        const marketCupHeader = createNewElement('div', ['coin-market-cup-header'], headerRow);
+        marketCupHeader.textContent = 'Объём';
+        const changeMarketCupHeader = createNewElement('div', ['coin-change-market-cup-header'], headerRow);
+        changeMarketCupHeader.textContent = 'Объём 24h %';
+
+        this.controller.coinsList.coinsListFromApi.forEach((oneCoin: CoinMarketData) => {
+            const coinRow: HTMLElement = createNewElement('div', ['one_coin_container'], this.coinsListHTML);
+
+            CoinsListView.addCoinDescriptionHTML(oneCoin.market_cap_rank, coinRow);
+            const coinTitleBlock = createNewElement('div',['coin_description'], coinRow);
+            const coinImage = createNewElement('img',['coin-logo-img'],coinTitleBlock);
+            coinImage.setAttribute('src', oneCoin.image);
+            coinImage.setAttribute('alt', 'coin-logo');
+            CoinsListView.addCoinDescriptionHTML(`${oneCoin.name} - ${oneCoin.symbol.toUpperCase()}`, coinTitleBlock);
+            CoinsListView.addCoinDescriptionHTML(`$${oneCoin.current_price.toLocaleString()}`, coinRow);
+            CoinsListView.addCoinDescriptionHTML(`${oneCoin.price_change_percentage_24h}%`, coinRow);
+            CoinsListView.addCoinDescriptionHTML(`$${oneCoin.market_cap.toLocaleString()}`, coinRow);
+            CoinsListView.addCoinDescriptionHTML(`${oneCoin.market_cap_change_percentage_24h}%`, coinRow);
         });
     }
 
-    static addCoinDesctiptionHTML(txt: string | number, parent: HTMLElement, cls = 'coin_description') {
+    static addCoinDescriptionHTML(txt: string | number, parent: HTMLElement, cls = 'coin_description') {
         const tempHTML: HTMLElement = createNewElement('div', [cls], parent);
         tempHTML.innerText = String(txt);
+        return tempHTML;
     }
 }
