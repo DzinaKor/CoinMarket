6987d07aecc421f2a9c1f4efc9134a0363b1cab1
akazus <kazus0105@mail.ru>
2022-09-02 23:53:24 +0400

    feat: add currency switcher

--- src/Views/RunningLineView.ts
+++ src/Views/RunningLineView.ts
@@ -15,23 +15,23 @@ export default class RunningLineView {
         this.runningLineHTML.innerHTML = '';
     }
 
-    viewRunningLine(coins : TrendCoin[], exchangeRate: number) {
+    viewRunningLine(coins: TrendCoin[], exchangeRate: number) {
         this.runningLineHTML.innerHTML = '';
+
         coins.forEach((coin) => {
-            const trendCoin  = createNewElement('div', ['trend-coin'], this.runningLineHTML);
-            const coinScore = createNewElement('div', ['coin-score'],trendCoin);
+            const trendCoin = createNewElement('div', ['trend-coin'], this.runningLineHTML);
+            const coinScore = createNewElement('div', ['coin-score'], trendCoin);
             coinScore.textContent = `${coin.item.score + 1}`;
             const coinImg = createNewElement('img', ['coin-img'], trendCoin);
             coinImg.setAttribute('src', `${coin.item.small}`);
             coinImg.setAttribute('alt', `${coin.item.name} symbol`);
-            const coinInfo = createNewElement('div', ['coin-column'],trendCoin)
-            const coinName = createNewElement('div', ['coin-name'],coinInfo);
+            const coinInfo = createNewElement('div', ['coin-column'], trendCoin);
+            const coinName = createNewElement('div', ['coin-name'], coinInfo);
             coinName.textContent = `${coin.item.name}`;
-            const marketCapRank = createNewElement('div', ['market-cap-rank'],coinInfo);
+            const marketCapRank = createNewElement('div', ['market-cap-rank'], coinInfo);
             marketCapRank.textContent = `ранг: ${coin.item.market_cap_rank}`;
-            const  priceBtc = createNewElement('div', ['price-btc'],coinInfo);
-            priceBtc.textContent = `${this.controller.coinsList.currencyUnit + (coin.item.price_btc * exchangeRate).toFixed(4)}`;
+            const priceBtc = createNewElement('div', ['price-btc'], coinInfo);
+            priceBtc.textContent = `${this.controller.mainData.selectedCurrency.symbol + (coin.item.price_btc * exchangeRate).toFixed(4)}`;
         });
     }
-
 }
