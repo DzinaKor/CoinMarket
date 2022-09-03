2c38dc822504f078181e96ac948116c75add349c
akazus <kazus0105@mail.ru>
2022-09-03 21:49:47 +0400

    feat: add lang to chart

--- src/Models/LangData.ts
+++ src/Models/LangData.ts
@@ -1,6 +1,5 @@
-
 export type LangType = {
-    [key: string]: string | LangType ;
+    [key: string]: string | LangType;
 }
 
 const lang: LangType = {
@@ -8,17 +7,26 @@ const lang: LangType = {
         calc_header: 'Cryptocurrency calculator and converter',
         calc_placeholder: 'Enter amount to convert',
         calc_popular_exchanges: 'Popular options for converting cryptocurrencies',
-
+        chart_line_chart: 'Line Chart',
+        chart_candle_chart: 'Candle Chart',
+        chart_price: 'Price'
     },
     'ru': {
         calc_header: 'Калькулятор и конвертер криптовалют',
         calc_placeholder: 'Введите сумму для конвертации',
-        calc_popular_exchanges: 'Популярные варианты конвертации криптовалют'
+        calc_popular_exchanges: 'Популярные варианты конвертации криптовалют',
+        chart_line_chart: 'Линейный график',
+        chart_candle_chart: 'Свечной график',
+        chart_price: 'Цена:'
+
     },
     'by': {
         calc_header: '',
         calc_placeholder: '',
         calc_popular_exchanges: '',
+        chart_line_chart: '',
+        chart_candle_chart: '',
+        chart_price: ''
     }
 };
 
